4cd1c1c7e8bea7913a7aab23f9d1a7f9ea9929a5
llr392425595 <392425595@qq.com>
2018-04-22 11:30:58 +0800

    修改inputValidatorTest测试异常

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -3,6 +3,7 @@ package tw.core;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 import tw.validator.InputValidator;
 
@@ -10,48 +11,40 @@ import tw.validator.InputValidator;
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+  private InputValidator inputValidator;
+
+  @Before
+  public final void before() {
+    inputValidator = new InputValidator();
+  }
+
   @Test
   public void should_get_true_when_given_correct_numStr() {
-    InputValidator inputValidator = new InputValidator();
-
-    String numStr = "1 2 3 4";
-    boolean result = inputValidator.validate(numStr);
+    boolean result = inputValidator.validate("1 2 3 4");
     assertTrue(result);
   }
 
   @Test
   public void should_get_true_when_given_same_digit() {
-    InputValidator inputValidator = new InputValidator();
-
-    String numStr = "1 2 3 2";
-    boolean result = inputValidator.validate(numStr);
+    boolean result = inputValidator.validate("1 2 3 2");
     assertFalse(result);
   }
 
   @Test
   public void should_get_false_when_given_num_is_not_single_digit() {
-    InputValidator inputValidator = new InputValidator();
-
-    String numStr = "1 11 3 4";
-    boolean result = inputValidator.validate(numStr);
+    boolean result = inputValidator.validate("1 11 3 4");
     assertFalse(result);
   }
 
   @Test
   public void should_get_false_when_given_digits_count_is_less_than_4() {
-    InputValidator inputValidator = new InputValidator();
-
-    String numStr = "1 2 3 ";
-    boolean result = inputValidator.validate(numStr);
+    boolean result = inputValidator.validate("1 2 3 ");
     assertFalse(result);
   }
 
   @Test
   public void should_get_false_when_given_digits_count_is_more_than_4() {
-    InputValidator inputValidator = new InputValidator();
-
-    String numStr = "1 2 3 4 5";
-    boolean result = inputValidator.validate(numStr);
+    boolean result = inputValidator.validate("1 2 3 4 5");
     assertFalse(result);
   }
 
