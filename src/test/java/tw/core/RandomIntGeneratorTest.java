c8a9199a8cbe9eef4e30f480894a2591fe3a324d
llr392425595 <392425595@qq.com>
2018-05-01 23:53:31 +0800

    refactor RandomGeneratorTest

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -4,6 +4,7 @@ package tw.core;
 import static junit.framework.TestCase.assertTrue;
 
 import java.util.concurrent.ExecutionException;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -14,22 +15,26 @@ import tw.core.generator.RandomIntGenerator;
  */
 public class RandomIntGeneratorTest {
 
+  private RandomIntGenerator randomIntGenerator;
+
   @Rule
   public ExpectedException thrown = ExpectedException.none();
+
+  @Before
+  public void setUp() {
+    randomIntGenerator = new RandomIntGenerator();
+  }
 
   @Test
   public void should_throw_IllegalArgumentException_when_digitmax_less_than_numbersOfNeed() {
     thrown.expect(IllegalArgumentException.class);
     thrown.expectMessage("Can't ask for more numbers than are available");
-    RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
     randomIntGenerator.generateNums(3, 4);
   }
 
   @Test
   public void should_get_a_numStr_which_length_is_7() {
-    RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
     String numStr = randomIntGenerator.generateNums(9, 4);
-
     assertTrue(numStr.length() == 7);
   }
 }
