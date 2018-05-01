f84e0fcc60543c41ea14adea650533ccdb4f6f17
llr392425595 <392425595@qq.com>
2018-05-02 00:35:43 +0800

    AnswerGeneratorTest

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,11 +1,14 @@
 package tw.core.generator;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import tw.core.Answer;
 import tw.core.exception.OutOfRangeAnswerException;
 
@@ -13,10 +16,24 @@ import tw.core.exception.OutOfRangeAnswerException;
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
-  @Test(expected = OutOfRangeAnswerException.class)
+
+  private RandomIntGenerator mockedRandomIntGenerator;
+
+
+  @Before
+  public void setUp() {
+    mockedRandomIntGenerator = mock(RandomIntGenerator.class);
+  }
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
   public void should_throw_outOfRangeAnswerException_when_digit_more_than_9()
       throws OutOfRangeAnswerException {
-    RandomIntGenerator mockedRandomIntGenerator = mock(RandomIntGenerator.class);
+    thrown.expect(OutOfRangeAnswerException.class);
+    thrown.expectMessage("Answer format is incorrect");
+
     when(mockedRandomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("1 2 3 11");
 
     AnswerGenerator answerGenerator = new AnswerGenerator(mockedRandomIntGenerator);
@@ -26,13 +43,13 @@ public class AnswerGeneratorTest {
   @Test
   public void should_return_a_numList()
       throws OutOfRangeAnswerException {
-    RandomIntGenerator mockedRandomIntGenerator = mock(RandomIntGenerator.class);
+
     when(mockedRandomIntGenerator.generateNums(anyInt(), anyInt())).thenReturn("1 2 3 4");
 
     AnswerGenerator answerGenerator = new AnswerGenerator(mockedRandomIntGenerator);
     Answer answer = answerGenerator.generate();
 
-    assertTrue(answer.getIndexOfNum("2") == 1);
+    Assert.assertEquals("1 2 3 4", answer.toString());
   }
 }
 
