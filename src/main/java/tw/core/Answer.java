4d76f38f6497b042d4c8c4dad20c2648434c6b08
llr392425595 <392425595@qq.com>
2018-05-02 00:45:19 +0800

    repaire Answer.validate()

--- src/main/java/tw/core/Answer.java
+++ src/main/java/tw/core/Answer.java
@@ -14,52 +14,54 @@ import static java.lang.Integer.parseInt;
  */
 public class Answer {
 
-    private static final String SPACE = " ";
-    private static final String ANSWER_INCORRECT = "Answer format is incorrect";
-    private List<String> numList;
+  private static final String SPACE = " ";
+  private static final String ANSWER_INCORRECT = "Answer format is incorrect";
+  private List<String> numList;
 
-    public void setNumList(List<String> numList) {
-        this.numList = numList;
-    }
+  public void setNumList(List<String> numList) {
+    this.numList = numList;
+  }
 
-    public Answer(List<String> numList) {
-        this.numList = numList;
-    }
+  public Answer(List<String> numList) {
+    this.numList = numList;
+  }
 
-    public static Answer createAnswer(String inputStr) {
-        List<String> inputList = Arrays.stream(inputStr.split(SPACE)).collect(Collectors.toList());
-        return new Answer(inputList);
-    }
+  public static Answer createAnswer(String inputStr) {
+    List<String> inputList = Arrays.stream(inputStr.split(SPACE)).collect(Collectors.toList());
+    return new Answer(inputList);
+  }
 
-    public void validate() throws OutOfRangeAnswerException {
-        if (numList.size() > numList.stream()
-                .map(Integer::parseInt)
-                .distinct()
-                .filter(num -> num < 10).count()) {
-            throw new OutOfRangeAnswerException(ANSWER_INCORRECT);
-        }
+  public void validate() throws OutOfRangeAnswerException {
+    if (numList.size() > numList.stream()
+        .map(Integer::parseInt)
+        .distinct()
+        .filter(num -> num < 10).count() || numList.size() != 4) {
+      throw new OutOfRangeAnswerException(ANSWER_INCORRECT);
     }
+  }
 
-    public Record check(Answer inputAnswer) {
-        Record record = new Record();
-        this.numList.forEach(num -> {
-            int index = inputAnswer.getIndexOfNum(num);
-            if (index == -1) return;
-            if (index == getIndexOfNum(num)) {
-                record.increaseCurrentNum();
-            } else {
-                record.increaseIncludeOnlyNum();
-            }
-        });
-        return record;
-    }
+  public Record check(Answer inputAnswer) {
+    Record record = new Record();
+    this.numList.forEach(num -> {
+      int index = inputAnswer.getIndexOfNum(num);
+      if (index == -1) {
+        return;
+      }
+      if (index == getIndexOfNum(num)) {
+        record.increaseCurrentNum();
+      } else {
+        record.increaseIncludeOnlyNum();
+      }
+    });
+    return record;
+  }
 
-    private int getIndexOfNum(String num) {
-        return this.numList.indexOf(num);
-    }
+  private int getIndexOfNum(String num) {
+    return this.numList.indexOf(num);
+  }
 
-    @Override
-    public String toString() {
-        return String.join(SPACE, numList);
-    }
+  @Override
+  public String toString() {
+    return String.join(SPACE, numList);
+  }
 }
