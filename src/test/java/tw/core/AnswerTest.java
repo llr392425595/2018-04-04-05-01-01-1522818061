6bd7ef020e5d778ee21dddb790828c6394e680b1
llr392425595 <392425595@qq.com>
2018-05-02 10:59:15 +0800

    refactor AnswerTest

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -3,19 +3,72 @@ package tw.core;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import tw.core.exception.OutOfRangeAnswerException;
 import tw.core.model.Record;
 
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
-  private Answer answer;
+  private Answer correctAnswer;
+  private Answer excessOfQuantityAnswer;
+  private Answer containLettersAnswer;
+  private Answer repeatingElementsAnswer;
 
   @Before
   public void setUp() throws Exception {
-    answer = Answer.createAnswer("1 2 3 4");
+    correctAnswer = Answer.createAnswer("1 2 3 4");
+    excessOfQuantityAnswer = Answer.createAnswer("1 2 3 4 5");
+    containLettersAnswer = Answer.createAnswer("1 2 3 a");
+    repeatingElementsAnswer = Answer.createAnswer("1 2 3 3 4");
+  }
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void createAnswerTest() {
+    Assert.assertEquals(correctAnswer.toString(),"1 2 3 4");
+    Assert.assertEquals(excessOfQuantityAnswer.toString(),"1 2 3 4 5");
+    Assert.assertEquals(containLettersAnswer.toString(),"1 2 3 a");
+    Assert.assertEquals(repeatingElementsAnswer.toString(),"1 2 3 3 4");
+  }
+
+  @Test
+  public void validate_correctAnswer_should_not_throw_exception() throws OutOfRangeAnswerException {
+    correctAnswer.validate();
+  }
+
+  @Test
+  public void validate_excessOfQuantityAnswer_should_throw_OutOfRangeAnswerException() throws OutOfRangeAnswerException {
+    thrown.expect(OutOfRangeAnswerException.class);
+    thrown.expectMessage("Answer format is incorrect");
+    excessOfQuantityAnswer.validate();
+  }
+
+  @Test
+  public void validate_containLettersAnswer_should_throw_OutOfRangeAnswerException() throws OutOfRangeAnswerException {
+    thrown.expect(NumberFormatException.class);
+    containLettersAnswer.validate();
+  }
+
+  @Test
+  public void validate_repeatingElementsAnswer_should_throw_OutOfRangeAnswerException() throws OutOfRangeAnswerException {
+    thrown.expect(OutOfRangeAnswerException.class);
+    thrown.expectMessage("Answer format is incorrect");
+    repeatingElementsAnswer.validate();
+  }
+
+  private void validateInputAnswerStr(String inputAnswerStr,String expectValue) {
+    Answer inputAnswer = Answer.createAnswer(inputAnswerStr);
+    Record result = correctAnswer.check(inputAnswer);
+    String res = result.toString();
+    assertThat(res, is(expectValue));
   }
 
   @Test
@@ -56,13 +109,5 @@ public class AnswerTest {
 
     //then
     validateInputAnswerStr(inputAnswerStr,expectValue);
-  }
-
-  private void validateInputAnswerStr(String inputAnswerStr,String expectValue) {
-    Answer inputAnswer = Answer.createAnswer(inputAnswerStr);
-    Record result = answer.check(inputAnswer);
-    int[] existRecord = result.getValue();
-    String res = String.format("%1$sA%2$sB", existRecord[0], existRecord[1]);
-    assertThat(res, is(expectValue));
   }
 }
